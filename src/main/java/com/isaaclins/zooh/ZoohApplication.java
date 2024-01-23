050f24848b379c0fa96f3a0663133c3ce65b6db7
isaaclins <isaac.lins07@gmail.com>
2024-01-23 14:25:10 +0100

    ADDED MAIN FILES!

---
+++ src/main/java/com/isaaclins/zooh/ZoohApplication.java
@@ -0,0 +1,13 @@
+package com.isaaclins.zooh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZoohApplication{
+
+    public static void main(String[] args){
+        SpringApplication.run(ZoohApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/isaaclins/zooh/ZoohApplicationTests.java
@@ -0,0 +1,13 @@
+package com.isaaclins.zooh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZoohApplicationTests{
+
+    @Test
+    void contextLoads(){
+    }
+
+}
