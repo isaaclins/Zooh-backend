c9e9b0e9813668c3dd137bebdcfd189c126ff651
isaaclins <isaac.lins07@gmail.com>
2024-01-23 15:51:25 +0100

    Fixed some shi....ps ships yup i love repairing boats :)

--- src/main/java/com/isaaclins/zooh/controller/ZoohController.java
+++ src/main/java/com/isaaclins/zooh/controller/ZoohController.java
@@ -1,10 +1,15 @@
 package com.isaaclins.zooh.controller;
 
 import com.isaaclins.zooh.entity.UserEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import static com.isaaclins.zooh.db.Generalisation.*;
+
+
+@Controller
 public class ZoohController{
 
 
@@ -12,30 +17,29 @@ public class ZoohController{
 
 
     @PostMapping("/register")
-    public String processRegistration(@RequestParam String username, @RequestParam int pin, @RequestParam(required = false) boolean isAdmin, Model model) {
+    public String processRegistration(@RequestParam String username, @RequestParam String password, @RequestParam(required = false) boolean isAdmin, Model model) {
         if (checkForUsername(username)) {
-            return "redirect:/username-error"; // Redirect to a failure page
+            return "redirect:/usernamealreadyexists"; // Redirect to a failure page
         } else {
             // Create a new UserEntity and save it to the database
             UserEntity savedUserToDB = new UserEntity();
             savedUserToDB.setUsername(username);
-            savedUserToDB.setPin(pin);
+            savedUserToDB.setPassword(password);
 
-            if (Registration.Register(savedUserToDB)) {
+            if (Register(savedUserToDB)) {
                 // Pass the user data to the success page
                 UserEntity displayedUserToWebsite = getUserByUsername(username);
                 if (displayedUserToWebsite != null) {  // Check if newUser is not null
                     String newusername = displayedUserToWebsite.getUsername();
-                    int newpin = displayedUserToWebsite.getPin();
+                    String newpin = displayedUserToWebsite.getPassword();
 
                     model.addAttribute("username", newusername);
-                    model.addAttribute("pin", newpin);
-                    model.addAttribute("isAdmin", newisAdmin ? "Admin" : "Not Admin");
+                    model.addAttribute("password", newpin);
                     return "registration-success"; // Redirect to a success page
                 } else {
                     UserEntity UserDataTakenFromDB = getUserByUserID(savedUserToDB.getUserID());
                     model.addAttribute("username", UserDataTakenFromDB.getUsername());
-                    model.addAttribute("pin", UserDataTakenFromDB.getPin());
+                    model.addAttribute("password", UserDataTakenFromDB.getPassword());
                     return "registration-success"; // take the userID from the submitted user by username, search data from DB and give to user as website
 
                 }
@@ -44,4 +48,5 @@ public class ZoohController{
             }
         }
     }
+
 }
