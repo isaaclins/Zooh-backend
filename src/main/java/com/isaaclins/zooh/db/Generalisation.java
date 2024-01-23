c415724834b791e9a1692e0caa6716661558ce37
isaaclins <isaac.lins07@gmail.com>
2024-01-23 15:54:01 +0100

    getUserByUsername added

--- src/main/java/com/isaaclins/zooh/db/Generalisation.java
+++ src/main/java/com/isaaclins/zooh/db/Generalisation.java
@@ -44,7 +44,50 @@ public class Generalisation{
         }
     }
 
+    public static UserEntity getUserByUsername(String username){
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
 
+        try {
+            connection = DBConnection.connectToDB();
+            String selectQuery = "SELECT * FROM user WHERE username = ?";
+            preparedStatement = connection.prepareStatement(selectQuery);
+            preparedStatement.setString(1, username);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                UserEntity user = new UserEntity();
+                user.setUserID(resultSet.getInt("userID"))
+                        .setUsername(resultSet.getString("username"))
+                        .setPassword(resultSet.getString("password"));
+                return user;
+            } else {
+                return null; // No user found with the given userID
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null; // Return null in case of an error
+
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
     public static boolean checkForUsername(String username) {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
@@ -116,6 +159,60 @@ public class Generalisation{
             }
         }
     }
+
+
+
+
+
+    public static UserEntity getUserByUserID(int userID) {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DBConnection.connectToDB();
+            String selectQuery = "SELECT * FROM user WHERE userID = ?";
+            preparedStatement = connection.prepareStatement(selectQuery);
+            preparedStatement.setInt(1, userID);
+
+            resultSet = preparedStatement.executeQuery();
+
+            if (resultSet.next()) {
+                UserEntity user = new UserEntity();
+                user.setUserID(resultSet.getInt("userID"))
+                        .setUsername(resultSet.getString("username"))
+                        .setPassword(resultSet.getString("password"));
+                return user;
+            } else {
+                return null; // No user found with the given userID
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null; // Return null in case of an error
+
+        } finally {
+            try {
+                if (resultSet != null) {
+                    resultSet.close();
+                }
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+                if (connection != null) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+
+
+
+
 
 // ANTI SQL-INJECTION!!!! LETS GOOOO
     public static String sanitizeAndEscape(String stringtosanitize) {
