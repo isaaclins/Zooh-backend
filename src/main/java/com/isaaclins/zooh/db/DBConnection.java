d4928d10f70d642c8ab10878df1a262c535289a9
isaaclins <isaac.lins07@gmail.com>
2024-01-23 14:26:53 +0100

    A tale of Dragons, Heroes and Legends...

---
+++ src/main/java/com/isaaclins/zooh/db/DBConnection.java
@@ -0,0 +1,21 @@
+package com.isaaclins.zooh.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection{
+    private static final String URL = "jdbc:mysql://localhost:3306/zooh?useSSL=false&allowPublicKeyRetrieval=true";
+    private static final String USER = "root";
+    private static final String PASSWORD = "220563";
+
+    public static Connection connectToDB() {
+        Connection Connection = null;
+        try {
+            Connection = DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (SQLException error) {
+            error.printStackTrace();
+        }
+        return Connection;
+    }
+}
