dc78ac65c73f4fdc1d96f1e2fa333b4cc3dc9f33
Isaac Lins <isaac.lins07@gmail.com>
2024-01-24 21:29:50 +0100

    consistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistencyconsistency

--- backend/src/main/java/com/isaaclins/zooh/repository/UserRepository.java
+++ backend/src/main/java/com/isaaclins/zooh/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.isaaclins.zooh.repository;
 
 import com.isaaclins.zooh.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository <UserEntity, Integer>{
+public interface UserRepository extends CrudRepository<UserEntity, Integer> {
 }
