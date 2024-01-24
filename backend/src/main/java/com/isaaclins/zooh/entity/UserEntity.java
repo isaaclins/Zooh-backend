2ee45703fa9e010bd8650812f9c50285aabc0592
isaaclins <isaac.lins07@gmail.com>
2024-01-24 16:06:07 +0100

    yolo push

--- backend/src/main/java/com/isaaclins/zooh/entity/UserEntity.java
+++ backend/src/main/java/com/isaaclins/zooh/entity/UserEntity.java
@@ -1,15 +1,7 @@
 package com.isaaclins.zooh.entity;
 
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import org.springframework.lang.NonNull;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Getter
 @Setter
@@ -20,8 +12,7 @@ import org.springframework.lang.NonNull;
 public class UserEntity {
 
     @Id
-    @NonNull
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int userID;
 
     @NonNull
@@ -30,31 +21,4 @@ public class UserEntity {
     @NonNull
     private String password;
 
-    public int getUserID(){
-        return userID;
-    }
-
-    @NonNull
-    public String getUsername(){
-        return username;
-    }
-
-    public String getPassword(){
-        return password;
-    }
-
-    public UserEntity setUserID(int userID){
-        this.userID = userID;
-        return this;
-    }
-
-    public UserEntity setUsername(@NonNull String username){
-        this.username = username;
-        return this;
-    }
-
-    public UserEntity setPassword(String password){
-        this.password = password;
-        return this;
-    }
 }
