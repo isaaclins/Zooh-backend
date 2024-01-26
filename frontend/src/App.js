b507df3f96917d45867719cac6ffe6e230c47a55
Patrick <37662345+patrick-ita00@users.noreply.github.com>
2024-01-26 10:19:28 +0100

    added some react components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import ZoohCard from './components/ZoohCard';
+import logo from './imgs/img1.jpg';
+import ZoohNavBar from './components/ZoohNavBar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+      <ZoohNavBar/>
+    <div className="App d-flex flex-row">
+      <ZoohCard text="test" image={logo}/>
+      <ZoohCard text="test2" image={logo}/>
     </div>
+
+    
+    </>
   );
 }
 
