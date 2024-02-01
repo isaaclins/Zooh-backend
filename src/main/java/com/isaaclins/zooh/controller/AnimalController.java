3a8ce36650d475f4d8b3d9de6afe77f739c197a9
Isaac Lins <isaac.lins07@gmail.com>
2024-02-01 13:49:34 +0100

    create postmapping animalEntity

--- src/main/java/com/isaaclins/zooh/controller/AnimalController.java
+++ src/main/java/com/isaaclins/zooh/controller/AnimalController.java
@@ -3,9 +3,7 @@ package com.isaaclins.zooh.controller;
 import com.isaaclins.zooh.entity.AnimalEntity;
 import com.isaaclins.zooh.repository.AnimalRepository;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,4 +22,9 @@ public class AnimalController {
         List<AnimalEntity> animals = animalRepository.findAll();
         return ResponseEntity.ok(animals);
     }
+    @PostMapping("/create")
+    public AnimalEntity createAnimal(@RequestBody AnimalEntity animal) {
+        return animalRepository.save(animal);
+    }
+
 }
