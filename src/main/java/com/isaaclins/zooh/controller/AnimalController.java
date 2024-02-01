f2f579b2d31b82d586fa98b91a9b0c53769214f1
Isaac Lins <isaac.lins07@gmail.com>
2024-02-01 14:03:06 +0100

    added putmapping editing
    lol

--- src/main/java/com/isaaclins/zooh/controller/AnimalController.java
+++ src/main/java/com/isaaclins/zooh/controller/AnimalController.java
@@ -1,11 +1,14 @@
 package com.isaaclins.zooh.controller;
 
 import com.isaaclins.zooh.entity.AnimalEntity;
+import com.isaaclins.zooh.entity.AnimalEntity;
+import com.isaaclins.zooh.entity.AnimalEntity;
 import com.isaaclins.zooh.repository.AnimalRepository;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/animal")
@@ -27,4 +30,22 @@ public class AnimalController {
         return animalRepository.save(animal);
     }
 
+    @PutMapping("/update/{id}")
+    public AnimalEntity updateAnimal(@PathVariable int id, @RequestBody AnimalEntity updatedAnimal) {
+        AnimalEntity existingAnimal = animalRepository.findById(id).orElse(null);
+        if (existingAnimal != null) {
+            existingAnimal.setAge(updatedAnimal.getAge());
+            existingAnimal.setName(updatedAnimal.getName());
+            existingAnimal.setSpecie(updatedAnimal.getSpecie());
+            existingAnimal.setLocationId(updatedAnimal.getLocationId());
+            return animalRepository.save(existingAnimal);
+        }
+        return null;
+    }
+    @GetMapping("/get/{id}")
+    public ResponseEntity<AnimalEntity> getAnimalById(@PathVariable int id) {
+        Optional<AnimalEntity> animalOptional = animalRepository.findById(id);
+        return animalOptional.map(ResponseEntity::ok).orElse(ResponseEntity.notFound().build());
+    }
+    
 }
