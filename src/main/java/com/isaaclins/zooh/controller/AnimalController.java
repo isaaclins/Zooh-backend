a3586b07359316a3665c82b1d7d21d9e9dd66b8f
Isaac Lins <isaac.lins07@gmail.com>
2024-02-01 12:45:52 +0100

    added animalcontroller and fixed pom.xml

--- src/main/java/com/isaaclins/zooh/controller/AnimalController.java
+++ src/main/java/com/isaaclins/zooh/controller/AnimalController.java
@@ -1,4 +1,27 @@
 package com.isaaclins.zooh.controller;
 
+import com.isaaclins.zooh.entity.AnimalEntity;
+import com.isaaclins.zooh.repository.AnimalRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/animal")
 public class AnimalController {
+
+    private final AnimalRepository animalRepository;
+
+    public AnimalController(AnimalRepository animalRepository) {
+        this.animalRepository = animalRepository;
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<AnimalEntity>> getAllAnimals() {
+        List<AnimalEntity> animals = animalRepository.findAll();
+        return ResponseEntity.ok(animals);
+    }
 }
